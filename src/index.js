abec397ddc98cc4a9b7bdfa67ce3626bef755a39
vetalmolotov <vetal.molotov@gmail.com>
2023-06-16 17:27:48 +0300

    task is finished

--- src/index.js
+++ src/index.js
@@ -1,26 +1,39 @@
 // Test import of a JavaScript module
-import { example } from '@/js/example'
+import {iconQuoteSVG} from './js/icon-quote'
+import {iconGalButtonSVG} from './js/icon-gallery-button'
+
+// import { example } from '@/js/example'
 
 // Test import of an asset
-import webpackLogo from '@/images/webpack-logo.svg'
+// import webpackLogo from '@/images/webpack-logo.svg'
 
 // Test import of styles
 import '@/styles/index.scss'
 
-// Appending to the DOM
-const logo = document.createElement('img')
-logo.src = webpackLogo
+// Insert SVG icons
+const iconQuote = document.querySelector('.icon-quote')
+iconQuote.innerHTML = iconQuoteSVG()
 
-const heading = document.createElement('h1')
-heading.textContent = example()
+const iconGalButton = document.querySelectorAll('.gallery-button')
+for (let i = 0; i < iconGalButton.length; ++i) {
+  iconGalButton[i].innerHTML = iconGalButtonSVG()
+}
+
+
+// Appending to the DOM
+// const logo = document.createElement('img')
+// logo.src = webpackLogo
+
+// const heading = document.createElement('h1')
+// heading.textContent = example()
 
 // Test a background image url in CSS
-const imageBackground = document.createElement('div')
-imageBackground.classList.add('image')
+// const imageBackground = document.createElement('div')
+// imageBackground.classList.add('image')
 
 // Test a public folder asset
-const imagePublic = document.createElement('img')
-imagePublic.src = '/assets/example.png'
+// const imagePublic = document.createElement('img')
+// imagePublic.src = '/assets/example.png'
 
-const app = document.querySelector('#root')
-app.append(logo, heading, imageBackground, imagePublic)
+// const app = document.querySelector('#root')
+// app.append(logo, heading, imageBackground, imagePublic)
